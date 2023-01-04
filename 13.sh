c789d087afb19412eb3bacbf3b666074b374ec3d
xvmvx <107462385+xvmvx@users.noreply.github.com>
2023-01-04 23:03:57 +0800

    Update 13.sh

--- 13.sh
+++ 13.sh
@@ -28,7 +28,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-docker version
+docker version || sudo apt  install docker.io 
     if [ $? = '0' ]; then
         green "安装完成✅✅✅！"
         read -p "继续安装docker-compose（y）："  dc
