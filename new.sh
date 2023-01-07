2bb57c0fd843825f7d29c61c691455bd0efbd9cc
xvmvx <107462385+xvmvx@users.noreply.github.com>
2023-01-07 19:42:25 +0800

    Update new.sh

--- new.sh
+++ new.sh
@@ -62,6 +62,8 @@ elif [[ ${MY} == "centos" ]]; then
   sudo yum update && sudo yum upgrade
   yum install wget git vim
 fi
+cp my.bashrc ~/.bashrc
+source ~/.bashrc
 sudo timedatectl set-timezone Asia/Shanghai #改成上海
 read -p "修改SSH端口号（y），确认修改：       " changeSSH
 if [[ "$changeSSH" = "y" ]] ; then
@@ -71,42 +73,77 @@ blue -e "请选择要安装的面板："
 yellow "1. 宝塔面板"
 yellow "2. AMH面板"
 yellow "3. DOCKER"
-yellow "4. NPM面板"
+yellow "4. webmin面板"
 read -e -p "(输入为空则取消):" inMY
-  if [[ ${inMY} == "1" ]]; then
+if [[ ${inMY} == "1" ]]; then
     echo -e "宝塔面板>>>要执行的操作："
- 	  echo "1. 全新安装"
+    echo "1. 全新安装"
     echo "2. 升级代码"
     echo "3. 还原到官方最新版"
     read -e -p "(输入为空则取消):" inBT
-      if [[ ${inBT} == "1" ]]; then
+    if [[ ${inBT} == "1" ]]; then
         if [[ ${MY} == "debian" ]]; then
-          # Debian全新安装命令：
-          wget -O install.sh http://v7.hostcli.com/install/install-ubuntu_6.0.sh && bash install.sh
+            # Debian全新安装命令：
+            wget -O install.sh http://v7.hostcli.com/install/install-ubuntu_6.0.sh && bash install.sh
         elif [[ ${MY} == "ubuntu" ]]; then
-          # ubuntu/Deepin全新安装命令：
-          wget -O install.sh http://v7.hostcli.com/install/install-ubuntu_6.0.sh && sudo bash install.sh
+            # ubuntu/Deepin全新安装命令：
+            wget -O install.sh http://v7.hostcli.com/install/install-ubuntu_6.0.sh && sudo bash install.sh
         elif [[ ${MY} == "centos" ]]; then
-          # Centos全新安装命令：根据系统执行框内命令开始安装（大约2分钟完成面板安装）升级后可能需要重启面板
-          yum install -y wget && wget -O install.sh http://v7.hostcli.com/install/install_6.0.sh && sh install.sh
+            # Centos全新安装命令：根据系统执行框内命令开始安装（大约2分钟完成面板安装）升级后可能需要重启面板
+            yum install -y wget && wget -O install.sh http://v7.hostcli.com/install/install_6.0.sh && sh install.sh
         else
-          # Fedora全新安装命令：
-          wget -O install.sh http://v7.hostcli.com/install/install_6.0.sh && bash install.sh
+            # Fedora全新安装命令：
+            wget -O install.sh http://v7.hostcli.com/install/install_6.0.sh && bash install.sh
         fi
-       elif [[ ${inBT} == "2" ]]; then
-          # 升级代码/修复面板：已经安装官方面板，执行下列命令升级到7.6.0纯净版：
-          curl http://v7.hostcli.com/install/update6.sh|bash
-          # 其他非官方版本含开心版、快乐版、纯净版等 7.4.5至7.6.0版本之间所有版本均可，执行下列命令升级到7.6.0纯净版：
-          curl http://v7.hostcli.com/install/update6.sh|bash
-       elif [[ ${inBT} == "3" ]]; then
-          # 任意非官方版本还原到官方最新版：
-          curl http://download.bt.cn/install/update6.sh|bash
-       fi
-     elif [[ ${inMY} == "2" ]]; then
-       # amh面板
-       wget http://dl.amh.sh/amh.sh && bash amh.sh
-     elif [[ ${inMY} == "3" ]]; then
-       # Docker
-       source 13.sh
-     fi
+    elif [[ ${inBT} == "2" ]]; then
+        # 升级代码/修复面板：已经安装官方面板，执行下列命令升级到7.6.0纯净版：
+        curl http://v7.hostcli.com/install/update6.sh|bash
+        # 其他非官方版本含开心版、快乐版、纯净版等 7.4.5至7.6.0版本之间所有版本均可，执行下列命令升级到7.6.0纯净版：
+        curl http://v7.hostcli.com/install/update6.sh|bash
+    elif [[ ${inBT} == "3" ]]; then
+        # 任意非官方版本还原到官方最新版：
+        curl http://download.bt.cn/install/update6.sh|bash
+    fi
+    if [ $? = '0' ]; then
+        green "宝塔安装完成✅✅✅！"
+    else
+        red "安装失败，人工检查！"
+    fi
+elif [[ ${inMY} == "2" ]]; then
+    # amh面板
+    wget http://dl.amh.sh/amh.sh && bash amh.sh
+    if [ $? = '0' ]; then
+        green "amh安装完成✅✅✅！"
+    else
+        red "安装失败，人工检查！"
+    fi
+elif [[ ${inMY} == "3" ]]; then
+    # Docker
+    source 13.sh
+    if [ $? = '0' ]; then
+        green "Docker安装完成✅✅✅！"
+    else
+        red "安装失败，人工检查！"
+    fi
+elif [[ ${inMY} == "4" ]]; then
+    if [[ ${MY} == "centos" ]]; then
+        (echo "[Webmin]
+        name=Webmin Distribution Neutral
+        baseurl=http://download.webmin.com/download/yum
+        enabled=1
+        gpgcheck=1
+        gpgkey=http://www.webmin.com/jcameron-key.asc" >/etc/yum.repos.d/webmin.repo;
+        yum -y install webmin)
+    else
+        wget -qO - http://www.webmin.com/jcameron-key.asc | sudo apt-key add -
+        sudo sh -c 'echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list'
+        sudo apt update
+        sudo apt install webmin
+    fi
+    if [ $? = '0' ]; then
+        green "Webmin安装完成✅✅✅！"
+    else
+        red "安装失败，人工检查！"
+    fi
+fi
      
